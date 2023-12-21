0d5ba0baf98aca0d7aa25ce33e362971f3ca10b0
Teddy Nueva Espana <97917873+tnuevaes@users.noreply.github.com>
2023-12-21 08:04:11 -0500

    Delete TestFunctions.vhd

--- TestFunctions.vhd
+++
@@ -1,7 +0,0 @@
-IF (bt_eq = '1' and choice='1') THEN
-	nx_acc <= acc ** operand;                   --Exponentiation, acc^operand
-	nx_state <= SHOW_RESULT;
-
-IF (bt_eq = '1' and choice='1') THEN
-	nx_acc <= acc mod operand;                   --Modulo
-	nx_state <= SHOW_RESULT;
